f14503b3e0566e6b90ce5d53ac64d2eac3e2205e
kailab28 <b.kaila@wustl.edu>
2026-03-31 02:11:59 +0000

    added another box with an x in it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 export default function Square() {
   return (
     <>
-      <button className="square">X</button>;
-      <button className="square">X</button>;
+      <button className="square">X</button>
+      <button className="square">X</button>
     </>
   );
-};
+}
